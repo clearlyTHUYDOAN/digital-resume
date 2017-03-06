1a5c1fb8b811ab504fb0673bf3464acea2fbc3bc
brainstation <clearlythuydoan@gmail.com>
2017-03-05 22:56:51 -0500

    test serve public

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + './../public'));
+// app.use(express.static(__dirname + './../public'));
+
+app.use(express.static('public'));
 
 app.listen(PORT,() => {
 	console.log("Listening on Port:%s",PORT)
